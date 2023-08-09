0d7f8075fd9f69b8c9b7d2c8e65407d4ca88d096
b-carr-88 <ben.carroll88@gmail.com>
2023-08-08 18:47:10 -0600

    gameState Addition

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
-var resourceCount = 0;
-
-var gatherMult = 1;
-var gatherMultCost = 100;
-
-var gatherers = 0;
-var gathererCost = 50;
-
-var hirelingMult = 1;
-var hirelingMultCost = 1000;
+const gameState = {
+    resourceCount: 0,
+    gatherMult: 1,
+    gatherMultCost: 100,
+    gatherers: 0,
+    gathererCost: 50,
+    hirelingMult: 1,
+    hirelingMultCost: 1000
+}
 
 setInterval(function() {
-    resourceCount += 1 * gatherers * hirelingMult;
-    document.getElementById("resource-count").innerHTML = resourceCount;
+    gameState.resourceCount += 1 * gameState.gatherers * gameState.hirelingMult;
+    document.getElementById("resource-count").innerHTML = gameState.resourceCount;
 }, 100)
 
 function gather() {
-    resourceCount += 1 * gatherMult;
-    document.getElementById("resource-count").innerHTML = resourceCount;
+    gameState.resourceCount += 1 * gameState.gatherMult;
+    document.getElementById("resource-count").innerHTML = gameState.resourceCount;
 }
 
 function gatherMultUp() {
-    if (costCheck(gatherMultCost)) {
-        gatherMult += 1;
-        resourceCount -= gatherMultCost;
-        gatherMultCost *= 1.3;
-        document.getElementById("mult-display").innerHTML = gatherMult;
+    if (costCheck(gameState.gatherMultCost)) {
+        gameState.gatherMult += 1;
+        gameState.resourceCount -= gameState.gatherMultCost;
+        gameState.gatherMultCost *= 1.3;
+        document.getElementById("mult-display").innerHTML = gameState.gatherMult;
     }
 }
 
 function buyGatherers() {
-    gatherers += 1;
-    document.getElementById("hire-count").innerHTML = gatherers;
+    gameState.gatherers += 1;
+    document.getElementById("hire-count").innerHTML = gameState.gatherers;
 }
 
 function hireMultUp() {
-    hirelingMult += 1;
-    document.getElementById("hireling-multiplier").innerHTML = hirelingMult;
+    gameState.hirelingMult += 1;
+    document.getElementById("hireling-multiplier").innerHTML = gameState.hirelingMult;
 }
 
 function costCheck(price) {
-    if((resourceCount - price) >= 0) {
+    if((gameState.resourceCount - price) >= 0) {
         return true;
     } else {
         return false;
