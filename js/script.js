ae5b1daeba4bba1fb0088f7503ee66b8fcf2c3d2
b-carr-88 <ben.carroll88@gmail.com>
2023-08-12 22:25:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,7 +96,8 @@ const gameLoop = setInterval(function() {
     document.getElementById("air-count").innerHTML = air.total.toFixed(2);
     document.getElementById("airbag-percent").innerHTML = ((air.total / airbagStats.cost) * 100).toFixed(2);
     if(air.total >= airbagStats.cost) {
-        var airbag = new airbagTemplate(airbagStats.cost)
+        var airbag = new airbagTemplate(airbagStats.cost);
+        window.airbag = airbag;
         air.total -= airbagStats.cost;
         airbag.runDungeon(dungeon1);
     }
